8ef6d6b8bcb1148e4fd71d9fd5ce8a5b3f1831a5
PraneethaGarladinne <praneethagarladinne@metavaratechnologies.com>
2026-06-06 09:49:10 +0530

    Migrated database to PostgreSQL

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,12 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./inventory.db"
+import os
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
